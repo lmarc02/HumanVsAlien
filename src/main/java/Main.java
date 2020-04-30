8ff0e750de64aac716b2b2ca047c0cb8874db1bd
lmarc02 <49591090+lmarc02@users.noreply.github.com>
2020-04-30 10:11:54 +0300

    update main file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args){
     Human human = new Human();
     Alien alien = new Alien();
+    Human human2 = new Human();
+    Alien alien2 = new Alien();
 
 
     alien.biteHuman(human);
