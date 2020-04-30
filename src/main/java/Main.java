062b252156b8c0b7501229a42482a7faf225f448
Loredana Marc <loredana.marc@softvision.ro>
2020-04-30 10:35:11 +0300

    main file update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args){
     Human human = new Human();
     Alien alien = new Alien();
-
+        Human human3 = new Human();
+        Alien alien3 = new Alien();
 
     alien.biteHuman(human);
     human.getHealth();
