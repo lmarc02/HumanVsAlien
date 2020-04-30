17499146e460ca88cac5a2faf62fea10989df118
lmarc02 <49591090+lmarc02@users.noreply.github.com>
2020-04-30 11:28:43 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args){
     Human human = new Human();
     Alien alien = new Alien();
-
-    Human human3 = new Human();
-    Alien alien3 = new Alien();
-
+        
     Human human2 = new Human();
     Alien alien2 = new Alien();
+        
+    Human human3 = new Human();
+    Alien alien3 = new Alien();
 
     Human human4 = new Human();
     Alien alien4 = new Alien();
