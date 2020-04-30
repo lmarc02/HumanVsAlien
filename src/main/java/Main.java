06f7fa86f640b32d1f1181374577de1586defd58
Loredana Marc <loredana.marc@softvision.ro>
2020-04-30 11:09:17 +0300

    main update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,14 @@ public class Main {
     Human human = new Human();
     Alien alien = new Alien();
 
-        Human human3 = new Human();
-        Alien alien3 = new Alien();
+    Human human3 = new Human();
+    Alien alien3 = new Alien();
 
     Human human2 = new Human();
     Alien alien2 = new Alien();
-        Human human4 = new Human();
-        Alien alien4 = new Alien();
+
+    Human human4 = new Human();
+    Alien alien4 = new Alien();
 
 
     alien.biteHuman(human);
