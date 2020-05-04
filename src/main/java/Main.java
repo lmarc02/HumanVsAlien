fa6db4f3086132aa363f6234cb9166cb22c62757
Loredana Marc <loredana.marc@softvision.ro>
2020-05-04 10:35:12 +0300

    removed 7th player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,7 @@ public class Main {
     Alien alien5 = new Alien();
         Human human6 = new Human();
         Alien alien6 = new Alien();
-
-    Human human7 = new Human();
-    Alien alien7 = new Alien();
+        
 
 
     alien.biteHuman(human);
