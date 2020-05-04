7f30a6d39ad3a680786baa3b5123829038a5a463
Loredana Marc <loredana.marc@softvision.ro>
2020-05-04 11:57:01 +0300

    first 5 players added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args){
     Human human = new Human();
     Alien alien = new Alien();
+
+    Human human2 = new Human();
+    Alien alien2 = new Alien();
         
     Human human3 = new Human();
     Alien alien3 = new Alien();
@@ -12,8 +15,6 @@ public class Main {
     Human human5 = new Human();
     Alien alien5 = new Alien();
 
-    Human human9 = new Human();
-    Alien alien9 = new Alien();
 
 
 
